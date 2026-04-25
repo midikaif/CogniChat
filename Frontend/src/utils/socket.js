6be114186f25123cc9ee2160f16ecf3b897784a0
Md Kaif <mdkaif0153@gmail.com>
2026-04-25 17:06:25 +0530

    fix(socket): update production socket URL for proper connection

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -1,7 +1,10 @@
 import {io} from "socket.io-client";
 
 export const connectSocket = () => {
-    const SOCKET_URL = import.meta.env.DEV ? "http://localhost:3000" : "/";
+    const SOCKET_URL = import.meta.env.DEV
+      ? "http://localhost:3000"
+      : "https://cognichat-fv23.onrender.com";
+
 
     return io(SOCKET_URL, {
       withCredentials: true,
